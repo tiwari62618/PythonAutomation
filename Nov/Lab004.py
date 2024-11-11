e46eed6d537b07152ac9e3e948178c5954783ac0
Mohit Tiwari <tiwari62618@gmail.com>
2024-11-11 11:38:34 +0530

    Initial commit

---
+++ Nov/Lab004.py
@@ -0,0 +1,16 @@
+None
+_ = 45
+_ = _ + 1
+print(_)
+print(type(_))
+age = 34
+AGE = 67
+print(type(age))
+print(type(AGE))
+age = ("were")
+print(type(age))
+a, b, c = 23, 34, 48
+print(a)
+print(b)
+print(c)
+print(a, b, c, sep="|")
---
+++ first.py
@@ -0,0 +1,7 @@
+print("hello"
+      )
+print(2 + 2)
+print("hello"
+      )
+print("Pramod", 123, "Amit", "john",sep='-',end=' ')
+print("Pramod", 123, "Amit", "john",sep='-',end='@')
