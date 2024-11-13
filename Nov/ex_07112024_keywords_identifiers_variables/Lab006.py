dd92a0b853513ede7925142b4223fdeb9a47932c
Mohit Tiwari <tiwari62618@gmail.com>
2024-11-13 11:47:04 +0530

    please add the code

--- Nov/ex_07112024_keywords_identifiers_variables/Lab006.py
+++ Nov/ex_07112024_keywords_identifiers_variables/Lab006.py
@@ -1,3 +1,11 @@
 first_name="Mohit"
 last_name="Tiwari"
 print(first_name,last_name)
+
+age=+2
+age1=-3
+print(age)
+print(age1)
+print(5%2)
+print(5//2)
+print(5/2)
---
+++ Nov/ex_07112024_keywords_identifiers_variables/Lab007.py
@@ -0,0 +1,4 @@
+print(2==2)
+print(2==4)
+a,b,c=2,3,4
+print(a,b,c)
