35c7d2ffbf9bc9adaa02cb0a1fb5bba5772f5449
Mohit Tiwari <tiwari62618@gmail.com>
2025-01-04 19:50:15 +0530

    please add the code

--- Nov/ex_12112024_Operators/Lab_task_1_ifelse.py
+++ Nov/ex_12112024_Operators/Lab_task_1_ifelse.py
@@ -2,7 +2,7 @@
 #Write a program to take a user age and let him know if he can go the club.  21
 
 age=int(input("enter your age:\n"))
-if age>=21:
+if age>21:
     print("You can go to the club")
 else:
     print("You can not go the club")
